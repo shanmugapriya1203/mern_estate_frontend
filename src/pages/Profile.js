ca7a20665f9fb5a453073f4be064e307bf7ab5d3
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-11 07:53:46 +0530

    create Signout api function

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,22 +3,26 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useRef } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart,updateUserSuccess,updateUserFailure } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess
+,signOutUserStart,signOutUserSuccess,signOutUserFailure } from '../redux/user/userSlice';
 import axios from 'axios';
 import { API_BASE_URL } from '../config';
+
 export default function Profile() {
-  const dispatch=useDispatch()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { currentUser } = useSelector((state) => state.user);
   const fileRef = useRef(null);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
- const[updateSuccess,setUpdateSuccess]=useState(false)
-  
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
   useEffect(() => {
     if (file) {
-      handleFileUpload(file); 
+      handleFileUpload(file);
     }
   }, [file]);
 
@@ -45,15 +49,15 @@ export default function Profile() {
       }
     );
   };
-  const handleChange=(e)=>{
-setFormData({...formData,[e.target.id]:e.target.value})
-  }
-  const handleSubmit= async (e)=>{
-    e.preventDefault()
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      dispatch(updateUserStart())
+      dispatch(updateUserStart());
       const response = await axios.post(
-        `${API_BASE_URL}/api/user/update/${currentUser._id}`,
+        `${API_BASE_URL}/api/user/update/${currentUser?._id}`,
         formData,
         {
           headers: {
@@ -62,24 +66,70 @@ setFormData({...formData,[e.target.id]:e.target.value})
         }
       );
       const data = response.data;
-    if (data.success === false) {
+      if (data.success === false) {
         const errorMessage = data.message || 'An unexpected error occurred.';
         window.alert(errorMessage);
         return;
       }
-  dispatch(updateUserSuccess(data));
-  setUpdateSuccess(true)
-  
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
     } catch (error) {
-      dispatch(updateUserFailure(error.message))
-  
+      dispatch(updateUserFailure(error.message));
     }
-  }
+  };
+  const handleDeleteUser = async () => {
+    const confirmed = window.confirm('Do you want to delete your account? This action cannot be undone.');
+  
+    if (confirmed) {
+      try {
+        dispatch(deleteUserStart());
+        const response = await axios.delete(
+          `${API_BASE_URL}/api/user/delete/${currentUser?._id}`,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+        const data = response.data;
+        if (data.success === false) {
+          const errorMessage = data.message || 'An unexpected error occurred.';
+          window.alert(errorMessage);
+          return;
+        }
+        dispatch(deleteUserSuccess(data));
+        navigate('/sign-in');
+      } catch (error) {
+        dispatch(deleteUserFailure(error.message));
+      }
+    }
+  };
 
+  
+  const handleSignOut = async () => {
+    const confirmed = window.confirm('Do you want to sign out?');
+  
+    if (confirmed) {
+      try {
+        dispatch(signOutUserStart());
+        const response = await axios.get(`${API_BASE_URL}/api/auth/signout`);
+        const data = response.data;
+        if (data.success === false) {
+          dispatch(signOutUserFailure(data.message));
+          return;
+        }
+        dispatch(signOutUserSuccess(data));
+        navigate('/sign-in')
+      } catch (error) {
+        dispatch(signOutUserFailure(error.message));
+      }
+    }
+  };
+  
   return (
     <main className='p-3 max-w-md mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form onSubmit= {handleSubmit} className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='file'
           ref={fileRef}
@@ -89,12 +139,12 @@ setFormData({...formData,[e.target.id]:e.target.value})
         />
 
         <img
-          src={formData.avatar || currentUser.avatar}
+          src={formData.avatar || (currentUser?.avatar || '')}
           alt='Profile'
           className='rounded-full h-16 w-16 object-cover cursor-pointer self-center mt-1'
           onClick={() => fileRef.current.click()}
         />
-               <p className='text-sm self-center'>
+        <p className='text-sm self-center'>
           {fileUploadError ? (
             <span className='text-red-700'>
               Error Image upload (image must be less than 2 mb)
@@ -110,8 +160,8 @@ setFormData({...formData,[e.target.id]:e.target.value})
 
         <label htmlFor='username'>Username</label>
         <input
-        defaultValue={currentUser.username}
-        onChange={handleChange}
+          defaultValue={currentUser?.username || ''}
+          onChange={handleChange}
           type='text'
           id='username'
           placeholder='Username'
@@ -119,8 +169,8 @@ setFormData({...formData,[e.target.id]:e.target.value})
         />
         <label htmlFor='email'>Email</label>
         <input
-        defaultValue={currentUser.email}
-        onChange={handleChange}
+          defaultValue={currentUser?.email || ''}
+          onChange={handleChange}
           type='email'
           id='email'
           placeholder='Email'
@@ -143,10 +193,10 @@ setFormData({...formData,[e.target.id]:e.target.value})
         </button>
       </form>
       <div className='flex justify-between mt-4'>
-        <span className='text-red-700 cursor-pointer'>Delete an account</span>
-        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+        <span className='text-red-700 cursor-pointer' onClick={handleDeleteUser}>Delete an account</span>
+        <span className='text-red-700 cursor-pointer' onClick={handleSignOut}>Sign Out</span>
       </div>
-   
+
       <p className='text-green-700 mt-5'>
         {updateSuccess ? 'User is updated successfully!' : ''}
       </p>
--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getAnalytics } from 'firebase/analytics';
 const initialState={
     currentUser:null,
    error:null,
@@ -29,8 +30,47 @@ updateUserStart:(state)=>{
 updateUserFailure:(state,action)=>{
     state.error= action.payload;
     state.loading=false
-}
+},
+deleteUserStart:(state)=>{
+    state.currentUser=null;
+    state.loading=false;
+    state.error=null
+},
+deleteUserSuccess:(state)=>{
+    state.currentUser=null;
+    state.loading=false;
+    state.error=null
+},
+deleteUserFailure:(state,action)=>{
+    state.error=action.payload;
+    state.loading= false
+}, signOutUserStart: (state) => {
+    state.loading = true;
+  },
+  signOutUserSuccess: (state) => {
+    state.currentUser = null;
+    state.loading = false;
+    state.error = null;
+  },
+  signOutUserFailure: (state, action) => {
+    state.error = action.payload;
+    state.loading = false;
+  },
     }
 })
-export const{ signInStart,signInSuccess,signInFailure,updateUserStart,updateUserFailure,updateUserSuccess}=userSlice.actions
+export const{ signInStart
+    ,signInSuccess,
+    signInFailure,
+    updateUserStart,
+    updateUserFailure,
+    updateUserSuccess,
+    deleteUserStart,
+    deleteUserSuccess,
+    deleteUserFailure,
+    signOutUserStart,
+    signOutUserSuccess,signOutUserFailure
+
+
+
+}=userSlice.actions
 export default userSlice.reducer;
