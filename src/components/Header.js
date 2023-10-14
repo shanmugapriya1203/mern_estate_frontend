6963fc35b868cb2ccfb49f3b3eda310ba48828c9
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-14 17:06:24 +0530

    changes in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,20 @@
-import React, { useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Link, useNavigate,useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const location= useLocation()
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate();
-  const location = useLocation();
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    const urlParams = new URLSearchParams(location.search);
+    const urlParams = new URLSearchParams(window.location.search);
     urlParams.set('searchTerm', searchTerm);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
-  }
+  };
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -24,21 +23,24 @@ export default function Header() {
       setSearchTerm(searchTermFromUrl);
     }
   }, [location.search]);
-
   return (
-    <header className='bg-slate-200 shadow-md sticky top-0 z-10'>
+    <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-          <h1 className='font-bold text-2xl text-slate-700 ml-2'>
-            <span className='text-slate-500'>Camp</span>
-            <span className='text-slate-700'>Lia</span>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
-        <form onSubmit={handleSubmit} className='bg-slate-300 p-3 rounded-md flex items-center'>
+        <form
+          onSubmit={handleSubmit}
+          className='bg-slate-100 p-3 rounded-lg flex items-center'
+        >
           <input
             type='text'
             placeholder='Search...'
-            className='bg-transparent focus:outline-none w-64 text-slate-600'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button>
@@ -46,27 +48,28 @@ export default function Header() {
           </button>
         </form>
         <ul className='flex gap-4'>
-          {/* Navigation Links */}
           <Link to='/'>
-            <li className='text-slate-700 hover:underline font-semibold'>Home</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline font-medium'>
+              Home
+            </li>
           </Link>
           <Link to='/about'>
-            <li className='text-slate-700 hover:underline font-semibold'>About</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline font-medium'>
+              About
+            </li>
           </Link>
           <Link to='/profile'>
             {currentUser ? (
-              <div className='flex items-center'>
-                <img
-                  className='rounded-full h-10 w-10 object-cover'
-                  src={currentUser.avatar}
-                  alt='profile'
-                />
-                <span className='text-slate-700 ml-2 uppercase font-extrabold'>{currentUser.username}</span>
-              </div>
+              <>
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+                 <span className='text-slate-700 ml-2 uppercase font-extrabold'>{currentUser.username}</span>
+              </>
             ) : (
-              <Link to='/sign-in'>
-                <li className='rounded-md bg-slate-500 text-white px-4 py-2 hover:bg-slate-700'> Sign in</li>
-              </Link>
+              <li className='rounded-md bg-slate-500 text-white px-4 py-2 hover:bg-slate-700'> Sign in</li>
             )}
           </Link>
         </ul>
